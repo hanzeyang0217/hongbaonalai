75f78d8898c85719a8251e615a08c4177f5c2d4c
Hanzeyang <hanzeyang0217@gmail.com>
2020-10-26 00:58:26 +0900

    标签图标貌似没改好改了一下 然后禁止了双击放大和双指缩放

--- src/main.js
+++ src/main.js
@@ -13,3 +13,20 @@ new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+
+let lastTouchEnd = 0;
+document.addEventListener('touchstart', function(event) {
+  if (event.touches.length > 1) {
+    event.preventDefault();
+  }
+});
+document.addEventListener('touchend', function(event) {
+  const now = (new Date()).getTime();
+  if (now - lastTouchEnd <= 300) {
+    event.preventDefault();
+  }
+  lastTouchEnd = now;
+}, false);
+document.addEventListener('gesturestart', function(event) {
+  event.preventDefault();
+});
