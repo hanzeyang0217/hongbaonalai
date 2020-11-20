99ebbb8b3a881adf8babb9d7e21c9fb7e39b215a
Hanzeyang <hanzeyang0217@gmail.com>
2020-11-20 11:48:51 +0900

    deploy

---
+++ precache-manifest.ba7216aa2bf61df0407b9b8f57756955.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8f2aa3589a0026fdeb6",
+    "url": "/hongbaonalai/css/about.5717bed2.css"
+  },
+  {
+    "revision": "261518bc1400203509a0",
+    "url": "/hongbaonalai/css/app.d4975442.css"
+  },
+  {
+    "revision": "bcea6140ab656a6f908179f7d4b3876f",
+    "url": "/hongbaonalai/index.html"
+  },
+  {
+    "revision": "f8f2aa3589a0026fdeb6",
+    "url": "/hongbaonalai/js/about.230dca85.js"
+  },
+  {
+    "revision": "261518bc1400203509a0",
+    "url": "/hongbaonalai/js/app.69e23412.js"
+  },
+  {
+    "revision": "1749a7d14aa29e251baa",
+    "url": "/hongbaonalai/js/chunk-vendors.c97a7ce9.js"
+  },
+  {
+    "revision": "bdfee1ea34059e8be567f4d667bf7d6d",
+    "url": "/hongbaonalai/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/hongbaonalai/robots.txt"
+  }
+]);
